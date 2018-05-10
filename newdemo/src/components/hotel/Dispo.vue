ffa9d2cd3876d7f96b5725ea108e0254407a08eb
mgperezquonext <mgperez@quonext-tur.com>
2018-05-10 17:24:35 +0200

    aa

--- newdemo/src/components/hotel/Dispo.vue
+++ newdemo/src/components/hotel/Dispo.vue
@@ -5,7 +5,7 @@
 
     <ul class="collection with-header">
       <li class="collection-header"><h4>{{ dispo.msg }}</h4></li>
-      <a v-on:click="abrirdetalle(hotel.hotelKey)" v-for="hotel in dispo.hotels" style="cursor: pointer;"><li class="collection-item"><div>{{ hotel.hotelName }}<a class="secondary-content">{{ hotel.hotelKey }}{{ hotel.bestDeal.retailPrice.value }} {{ hotel.bestDeal.retailPrice.currencyIsoCode }}</a></div></li></a>
+      <a v-on:click="abrirdetalle(hotel.hotelKey)" v-for="hotel in dispo.hotels" style="cursor: pointer;"><li class="collection-item"><div>{{ hotel.hotelName }}<a class="secondary-content">{{ hotel.bestDeal.retailPrice.value }} {{ hotel.bestDeal.retailPrice.currencyIsoCode }}</a></div></li></a>
     </ul>
 
   </div>
