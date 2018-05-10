30ab213919e766a6f2494991d4dfe1da1e0df4a0
mgperezquonext <mgperez@quonext-tur.com>
2018-05-10 09:13:50 +0200

    aa

--- newdemo/src/components/Reserva.vue
+++ newdemo/src/components/Reserva.vue
@@ -12,7 +12,7 @@
 
         <ul id="nav-mobile" class="right hide-on-med-and-down">
         <router-link :to="{name: 'hotel_formulario'}" tag="li" active-class="active"><a>Hotel</a></router-link>
-        <router-link :to="{name: 'traslado'}" tag="li" active-class="active"><a>Traslado</a></router-link>
+        <router-link :to="{name: 'traslado_formulario'}" tag="li" active-class="active"><a>Traslado</a></router-link>
         <router-link :to="{name: 'excursion_formulario'}" tag="li" active-class="active"><a>Excursion</a></router-link>
           <router-link :to="{name: 'login'}" tag="li" active-class="active"><a>Agent sign in</a></router-link>
         <router-link :to="{name: 'config'}" tag="li" active-class="active"><a>{{ baseurl }}</a></router-link>
@@ -22,7 +22,7 @@
 
     <ul class="sidenav" id="mobile-demo">
       <li><a v-on:click="ir('hotel_formulario')">Hotel</a></li>
-      <li><a v-on:click="ir('traslado')">Traslado</a></li>
+      <li><a v-on:click="ir('traslado_formulario')">Traslado</a></li>
       <li><a v-on:click="ir('excursion_formulario')">Excursion</a></li>
       <li><a v-on:click="ir('login')">Agent sign in</a></li>
       <li><a v-on:click="ir('config')">{{ baseurl }}</a></li>
