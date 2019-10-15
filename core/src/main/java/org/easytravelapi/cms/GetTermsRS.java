a604297442f494da84790079e736ba1c2281bd89
ramonnieto <rnieto@quonext.com>
2019-10-15 10:23:24 +0200

    add unmailing for CMS terms and conditions

--- core/src/main/java/org/easytravelapi/cms/GetTermsRS.java
+++ core/src/main/java/org/easytravelapi/cms/GetTermsRS.java
@@ -4,8 +4,13 @@ import org.easytravelapi.common.AbstractRS;
 
 public class GetTermsRS extends AbstractRS {
     private String terms;
+    private String unmailing;
 
     public String getTerms() { return terms; }
 
     public void setTerms(String terms) { this.terms = terms; }
+
+    public String getUnmailing() { return unmailing; }
+
+    public void setUnmailing(String unmailing) { this.unmailing = unmailing; }
 }
