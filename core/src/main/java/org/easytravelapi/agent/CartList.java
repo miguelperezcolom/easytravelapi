e849c6b005dfa9a3064e56f63d0d05a60b2c1b9a
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2020-06-29 16:02:05 +0200

    aa

--- core/src/main/java/org/easytravelapi/agent/CartList.java
+++ core/src/main/java/org/easytravelapi/agent/CartList.java
@@ -3,6 +3,13 @@ package org.easytravelapi.agent;
 import java.util.List;
 
 public class CartList {
+
+    private String fileId;
+
+    private String agencyReference;
+
+    private String productionCenterId;
+
     private String date;
 
     private Personaldata personaldata;
@@ -63,5 +70,27 @@ public class CartList {
         this.status = status;
     }
 
+    public String getFileId() {
+        return fileId;
+    }
 
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getAgencyReference() {
+        return agencyReference;
+    }
+
+    public void setAgencyReference(String agencyReference) {
+        this.agencyReference = agencyReference;
+    }
+
+    public String getProductionCenterId() {
+        return productionCenterId;
+    }
+
+    public void setProductionCenterId(String productionCenterId) {
+        this.productionCenterId = productionCenterId;
+    }
 }
